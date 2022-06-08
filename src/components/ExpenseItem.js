f119c0bee560b573fd9c7d1ad39eb3c849b1aaac
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-06-08 16:16:50 -0500

    more complex component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div>
+      <div>June 8 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
