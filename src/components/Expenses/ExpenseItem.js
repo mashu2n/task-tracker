63e355343dcd064dda48c40a4ac2478358077b53
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-06-20 12:27:35 -0500

    listening to user inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,6 +8,7 @@ const ExpenseItem = (props) => {
   // function clickHandler() {}
   const [title, setTitle] = useState(props.title);
   console.log("ExpenseItem evaluated by react");
+
   const clickHandler = () => {
     setTitle("Updated!");
     console.log(title);
