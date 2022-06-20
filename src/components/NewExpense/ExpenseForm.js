ed44deb31acb858c920b281a7b3adfe5954b860c
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-06-20 13:54:10 -0500

    updating state depends on previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,37 +3,40 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //const [enteredTitle, setEnteredTitle] = useState("");
-  //const [enteredAmount, setEnteredAmount] = useState("");
-  //const [enteredDate, setEnteredDate] = useState("");
-  useState[(userInput, setUserInput)]({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //useState[(userInput, setUserInput)]({
+  //enteredTitle: "",
+  // enteredAmount: "",
+  // enteredDate: "",
+  //});
 
   const titleChangeHandler = (event) => {
-    //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    // ...userInput,
+    //enteredTitle: event.target.value,
+    //  });
+    //setUserInput ((prevState)=> {
+    //  return { ...prevState, enteredTitle: event.target.value}
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    //setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //  ...userInput,
+    // enteredAmount: event.target.value,
+    //  });
   };
 
   const dateChangeHandler = (event) => {
-    //setEnteredDate(event.targer.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.targer.value);
+    // setUserInput({
+    //  ...userInput,
+    //  enteredDate: event.target.value,
+    // });
   };
 };
 
